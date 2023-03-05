5f84464a26e76f6b8f7a895b9e6a6e8e6abe0aae
huangbentai <583181266@qq.com>
2023-03-05 20:28:20 +0800

    项目初始化

---
+++ src/main/java/com/yisuo/yisuoplatfrom/YisuoPlatfromApplication.java
@@ -0,0 +1,13 @@
+package com.yisuo.yisuoplatfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YisuoPlatfromApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YisuoPlatfromApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yisuo/yisuoplatfrom/YisuoPlatfromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yisuo.yisuoplatfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YisuoPlatfromApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
